afd841e41250a0ebd879ead07bb7c68965a16ee4
Adeyin Ikpah <aikpaha@us.ibm.com>
2020-07-16 00:21:34 -0500

    Code to get ever other group of two in list

---
+++ get_two_group.py
@@ -0,0 +1,37 @@
+"""Get every other group of 2 numbers, if even"""
+
+
+def get_two_group(the_list):
+    new_list = []
+    next_list = []
+    final_list = []
+    a_list = []
+    the_group = []
+
+    for i in range(0, len(the_list), 2):
+        new_list.append(i)
+
+    for j in range(1, len(the_list), 2):
+        next_list.append(j)
+
+    # print(new_list)
+    # print(next_list)
+
+    for i, j in zip(new_list, next_list):
+        final_list.append([i, j])
+
+    # print(final_list)
+
+    for i in range(0, len(final_list), 2):
+        a_list.append(final_list[i])
+
+    # print(a_list)
+
+    for i in range(len(a_list)):
+        for j in range(2):
+            the_group.append(a_list[i][j])
+
+    return the_group
+
+
+print(get_two_group([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
