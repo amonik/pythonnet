abbfe33b837369539bf1fca78cb1b9398687c79b
Adeyin Ikpah <aikpaha@us.ibm.com>
2020-07-30 15:25:43 -0500

    update class

--- fib.py
+++ fib.py
@@ -12,3 +12,30 @@ def cal(n):
 if __name__ == '__main__':
     main()
 
+
+class Person:
+    def __init__(self):
+        self.__name = ""
+
+    def set_name(self, name):
+        self.__name = name
+
+    def get_name(self):
+        return self.__name
+
+       
+class Patient(Person):
+    def __init__(self, name, sickness):
+        self.__sickness = sickness
+        Person.__init__(name)
+
+    def set_sickness(self, sickness):
+        self.__sickness = sickness
+
+    def get_sickness(self):
+        return self.__sickness
+
+
+
+
+
