1456c4142b3f7e336807be28acdc16bbeee9b83a
Adeyin Ikpah <aikpaha@us.ibm.com>
2020-07-30 15:59:51 -0500

    update

--- fib.py
+++ fib.py
@@ -1,3 +1,4 @@
+"""
 def main():
     print(cal(8))
 
@@ -23,7 +24,7 @@ class Person:
     def get_name(self):
         return self.__name
 
-       
+
 class Patient(Person):
     def __init__(self, name, sickness):
         self.__sickness = sickness
@@ -39,6 +40,58 @@ class Patient(Person):
 class InvalidInput(ValueError):
     def __init__(self, my_input):
         super().__init__(f"You enter in incorrect value ${my_input}")
+
+
+
+from tkinter import *
+from tkinter import messagebox
+
+class WidgetDemo():
+    def __init__(self):
+        window = Tk()
+        window.title("Widget Demo!")
+        frame1 = Frame(window)
+        frame1.pack()
+        button1 = tk.F
+"""
+
+
+class InvalidInput(ValueError):
+    def __init__(self, my_input):
+        super().__init__(f"You enter in incorrect value {my_input}")
+        self.my_input = my_input
+
+
+class Area:
+    def __init__(self):
+        self.x = 0
+
+    def set_x(self, n):
+        if n <= 0:
+            raise InvalidInput(n)
+        else:
+            self.x = n
+
+    def __str__(self):
+        return str(self.x)
+
+
+def main():
+    a = Area()
+    input_value = int(input("Enter in a value greater than 0: "))
+    a.set_x(input_value)
+    if input_value <= 0:
+        print("Invalid input entered")
+    else:
+        print(a)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
 
 
 
